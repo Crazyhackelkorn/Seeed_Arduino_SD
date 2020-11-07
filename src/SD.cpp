c780b57f3d829f70f3f556cb3aa18af4a71ddcb5
https://Crazyhackelkorn@github.com <user.schrott@web.de>
2020-11-07 16:27:11 +0100

    Forget to add the new Files

---
+++ src/SD.cpp
@@ -0,0 +1,4 @@
+#include "Arduino.h"
+#include "SD.h"
+#include "Seees_SD.h"
+
---
+++ src/SD.h
@@ -0,0 +1,16 @@
+#ifndef SD_H
+#define SD_H
+
+#include "Arduino.h"
+
+class SD
+{
+public:
+	SD (arguments);
+	virtual ~SD ();
+
+private:
+	/* data */
+};
+
+#endif
